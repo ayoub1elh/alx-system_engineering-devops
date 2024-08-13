ca091f58bb84c99d81d6739578d3d9e03f15215a
ayoub1elh <ay2oub49@gmail.com>
2024-08-14 00:51:34 +0100

    fix

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -4,28 +4,36 @@
 the titles of the first 10
 """
 
-from requests import get
-
+import requests
 
 def top_ten(subreddit):
     """
-    function that queries the Reddit API """
-
+    function that queries the Reddit API
+    """
     if subreddit is None or not isinstance(subreddit, str):
         print("None")
+        return
 
-    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    # Reddit API now requires a more specific User-Agent
+    user_agent = {'User-agent': 'MyBot/0.0.1'}
     params = {'limit': 10}
-    url_base = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
+    
+    # Updated URL to use the OAuth endpoint
+    url = f'https://oauth.reddit.com/r/{subreddit}/hot'
 
-    ml = get(url_base, headers=user_agent, params=params)
-    total = ml.json()
+    # You need to replace 'YOUR_ACCESS_TOKEN' with a valid OAuth token
+    headers = {
+        **user_agent,
+        'Authorization': 'bearer YOUR_ACCESS_TOKEN'
+    }
 
     try:
-        my_data = total.get('data').get('children')
+        response = requests.get(url, headers=headers, params=params)
+        response.raise_for_status()  # Raises an HTTPError for bad responses
+        data = response.json()
 
-        for i in my_data:
-            print(i.get('data').get('title'))
+        for post in data.get('data', {}).get('children', []):
+            print(post.get('data', {}).get('title'))
 
     except Exception:
         print("None")
